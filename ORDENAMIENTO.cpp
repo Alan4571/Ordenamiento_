1b8cd9f8cb37eea791166bebdc45dd8381c939e0
DeyvidChinchilla <dchinchillae2@miumg.edu.gt>
2022-04-02 21:10:37 -0600

    Ordenamiento_

--- ORDENAMIENTO.cpp
+++ ORDENAMIENTO.cpp
@@ -204,3 +204,214 @@ switch (opcion)
 	system("Pause()");
 	}
 }
+//////////////////////////////////////	BURBUJA /////////////////////////////////////////////
+void Burbuja(){
+	int i,j,n;
+	cout<<"Indique la cantidad de numeros para el arreglo:"<<endl;
+	cin>>n;
+	cout<<""<<endl;
+	float numero[n];
+	float aux;
+	
+	cout<<"Ingrese los numeros a ordenar:"<<endl;
+	for (i=0; i<n; i++){
+		cout<<"numero"<<" "<<i+1<<":";
+		cin>>numero[i];
+	
+						}
+	for(j=0; j<n-1; j++)
+		for( i=0;i<n-1;i++){
+			if(numero[i] > numero[i+1])
+			{
+				aux = numero[i];
+				numero[i] = numero[i+1];
+				numero[i+1] = aux;
+
+			}
+							}
+	cout<<""<<endl;							
+	cout<<"Los numeros ordenados son:"<<endl;						
+	for(i=0; i<n; i++)
+	cout<<numero[i]<<endl;
+}
+
+////////////////////////////////////////////////  QUICK SORT ///////////////////////////////////////////////
+void QuickSort(){ 
+	int n;
+	cout << "Ingresar los datos" << endl;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Ingresar los datos " << i + 1 << " : " << endl;
+        cin >> a[i];
+    }
+    quickSort(a, n);
+    print(a, n);
+
+}
+
+void quickSort(int a[], int n)
+{
+    int tope, ini, fin, pos;
+    int may[MAX], menor[MAX];
+    tope = 0;
+    menor[tope] = 0;
+    may[tope] = n - 1;
+    while (tope >= 0)
+    {
+        ini = menor[tope];
+        fin = may[tope];
+        tope--;
+
+        int izq, der, aux;
+        bool band;
+        izq = ini;
+        der = fin;
+        pos = ini;
+
+        band = true;
+
+        while (band == true)
+        {
+            while ((a[pos] < a[der]) && (pos != der))
+                der--;
+
+            if (pos == der)
+                band = false;
+            else
+            {
+                aux = a[pos];
+                a[pos] = a[der];
+                a[der] = aux;
+                pos = der;
+            }
+
+            while ((a[pos] > a[izq]) && (pos != izq))
+                izq++;
+
+            if (pos == izq)
+                band = false;
+            else
+            {
+                aux = a[pos];
+                a[pos] = a[izq];
+                a[izq] = aux;
+                pos = izq;
+            }
+        }
+
+        if (ini <= (pos - 1))
+        {
+            tope++;
+            menor[tope] = ini;
+            may[tope] = pos - 1;
+        }
+
+        if (fin >= (pos + 1))
+        {
+            tope++;
+            menor[tope] = pos + 1;
+            may[tope] = fin;
+        }
+    }
+}
+
+void print(int a[], int n)
+{
+    cout << "Ordenar datos" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[" << a[i] << "]";
+    }
+}
+
+///////////////////////////////////////// SHELL SORT //////////////////////////////////////////
+void ShellSort(){
+	  int Num;
+    cout<<"Ingrese dimension del arreglo : ";
+    cin>>Num;
+    LeerArreglo(Num);
+    Shell(Num);
+    cout<<endl;
+    EscribeArreglo(Num);
+}
+void LeerArreglo(int Numero){
+    int i;
+    for(i=1;i<=Numero;i++)
+    {
+        cout<<"Arreglo["<<i<<"]=";
+        cin>>Arreglo[i];
+    }
+}
+
+void EscribeArreglo(int Numero){
+    int i;
+    cout<<"elementos ordenados por metodo Shell sort"<<endl;
+    for(i=1;i<=Numero;i++)
+    {
+        
+        cout<<"\t"<<Arreglo[i];
+    }
+    cout<<endl;
+}
+
+void Shell(int Numero){
+    int i,j,k,incremento,aux;
+    
+    incremento=Numero/2;
+    
+    while(incremento>0){
+        for(i=incremento+1;i<=Numero;i++){
+            j=i-incremento;
+            while(j>0){
+                if(Arreglo[j]>=Arreglo[j+incremento]){
+                    aux = Arreglo[j];
+                    Arreglo[j] = Arreglo[j+incremento];
+                    Arreglo[j+incremento] = aux;
+                }
+                else{
+                        j=0;
+                    }
+                j=j-incremento;
+            }
+        }
+        incremento=incremento/2;
+    }
+}
+
+/////////////////////////////////////////////// HEAP SORT //////////////////////////////////////////
+void HeapSort(){
+	int A[max],j,item,temp,i,k,n;
+    cout<<"Ingresa la cantidad de elementos del arreglo: ";
+    cin>>n;
+    cout<<endl;
+    cout<<"Ingrese los numeros a ordenar: "<<endl;
+   
+    for(i=1;i<=n;i++)
+    cin >> A[i];
+ 
+    for(k=n;k>0;k--)
+    {
+        for(i=1;i<=k;i++)
+        {
+            item=A[i];
+            j=i/2;
+            while(j>0 && A[j]<item)
+            {
+                A[i]=A[j];
+                i=j;
+                j=j/2;
+            }
+            A[i]=item;
+        }
+        temp=A[1];
+        A[1]=A[k];
+        A[k]=temp;
+    }
+    cout<<endl;
+    cout<<"El orden es:"<<endl;
+    for(i=1;i<=n;i++)
+    cout<<A[i] << endl;
+}
+
