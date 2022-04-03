9f000bc542974d78ab8e3d9af3f877d2939790d5
CristianPoroj <cporojj@miumg.edu.gt>
2022-04-02 21:35:33 -0600

    Ordenamiento_

--- ORDENAMIENTO.cpp
+++ ORDENAMIENTO.cpp
@@ -204,3 +204,204 @@ switch (opcion)
 	system("Pause()");
 	}
 }
+
+//////////////////////////////////////// MERGE SORT /////////////////////////////////////////////////
+void MergeSort(){
+	 int num,i;
+    cout<<"  *******************************"<<endl;
+    cout<<"   PROGRAMA MERGE SORT         "<<endl;
+    cout<<"  ******************************  "<<endl;
+    cout<<endl<<endl;
+    cout<<"INGRESE LA CANTIDAD DE ELEMENTOS"<<endl;
+    cin>>num;
+    cout<<endl;
+    cout<<"LOS ELEMENTOS SON:"<<endl;
+    for(i=1;i<=num;i++){
+        cin>>a[i] ;
+    }
+    merge_sort(1,num);
+    cout<<endl;
+    cout<<endl<<endl;
+    cout<<"EL NUEVO ORDEN DE ELEMENTOS ES:"<<endl;
+    for(i=1;i<=num;i++){
+        cout<<a[i]<<"   ";
+    }
+    cout<<endl;
+}
+
+//////////////////////////////////////////////////// SHAKER SORT ////////////////////////////////////////////////
+void ShakerSort(){
+	int i, k, Der, Izq, Aux, N, A[30];
+    cout<<"METODO DE ORDENAMIENTO - SHAKER SORT"<<endl;
+        
+            cout<<"Ingrese el tama?o del arreglo : "<<endl; 
+        cin>>N;
+        k=N;
+        Izq=2;
+        Der=N;
+        
+        for(i=1;i<=N;i++){
+            cout<<"\tA["<<i<<"] : ";        
+            cin>>A[i];
+        }
+        do{//inicio del ordenamiento
+            for(i=Der;i>=Izq;i--){//derecha a izquierda
+                if(A[i-1]>A[i]){
+                    Aux=A[i-1];
+                    A[i-1]=A[i];
+                    A[i]=Aux;
+                    k=i;
+                }
+        }
+            Izq=k+1;
+            for(i=Izq;i<=Der;i++)//izquierda a derecha
+                if(A[i-1]>A[i]){
+                    Aux=A[i-1];
+                    A[i-1]=A[i];
+                    A[i]=Aux;
+                    k=i;
+                }
+            Der=k-1;
+        }while(Izq<Der);//Fin del ordenamiento
+        
+        cout<<"\n\tArreglo Ordenado\n\t==================\n";
+        
+        for(i=1;i<=N;i++)
+            cout<<"\t"<<A[i];
+        cout<<endl<<"\t";
+        
+        system("pause");
+        getch();
+	
+	
+}
+
+//////////////////////////////////////////////// INSERCION DIRECTA /////////////////////////////////////
+void Inserciondirecta(){
+	 int n;
+    cout << "Ingrese el total de numeros : " << endl;
+    cin >> n;
+    int num[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Ingrese los numeros a ordenar:  " << i+1 << endl;
+        cin >> num[i];
+    }
+    directInsertion(num, n);
+    print(num, n);
+    cout<<endl;
+	
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void metodoBusqueda(){
+	int opcion;
+	char opc = 's';
+	while (opc == 's')
+	{
+        system("cls");
+	int opcion;
+	cout<<"Ingrese el tipo de Busqueda"<<endl;
+	cout<<"1-Secuencial"<<endl; 
+	cout<<"2-Binaria"<<endl;
+	cout<<"3-Diagrama E-R"<<endl;
+	cout<<"3-PARA REGRESAR"<<endl;
+	cin>>opcion;
+switch (opcion)
+	{
+		case 1:
+				Secuencial();
+		break;
+		case 2:
+				Binaria();
+		break;/*
+		case 3:
+				DiagramaE_R();
+		break;*/
+		case 3: 
+         		cout<<"Desea Regresar S/N ?\n"<<endl;
+                fflush(stdin);//It's a function which allows you to flush [clear] the input buffer. 
+				cin>>opc;
+		break;
+		}
+	system("Pause()");
+	}
+}
+void Secuencial(){
+ 	pedirDatos();
+    pedirNumero();
+    buscarDatos();
+    delete[] numeros;
+    system("pause");
+}
+
+void pedirDatos(){
+    std::cout<<"cuantos numeros va a tener tu arreglo:";
+    std::cin>>cantidadNumeros;
+    numeros=new int[cantidadNumeros];
+    for (int i = 0; i < cantidadNumeros; i++)
+    {
+        std::cout<<"Numero "<<i+1<<": ";
+        std::cin>>*(numeros+i);
+    }
+}
+void pedirNumero(){
+    std::cout<<"que numero quieres buscar de tu arreglo:";
+    std::cin>>numeroBuscar;
+}
+void buscarDatos(){
+    bool encontrado=false;
+    int i=0;
+    while (i<cantidadNumeros && encontrado==false)
+    {
+        if(*(numeros+i)==numeroBuscar){
+            encontrado=true;
+        }
+        i++;
+    }
+    if (encontrado==true)
+    {
+        std::cout<<"felicidades el numero "<<numeroBuscar<<" si se encuentra en el arreglo\n";
+    }
+    else{
+        std::cout<<"el numero "<<numeroBuscar<<" no esta en la lista \n";
+    }    
+}
+
+void Binaria(){
+	int numeros[] = {1,2,3,4,5};
+	int inf,sup,mitad,dato,i;
+	char band='F';
+	
+	dato = 3;
+	
+	//Algoritmo de la Busqueda Binaria
+	inf=0;
+	sup=5;
+	i=4;
+	while((inf<=sup)&&(i<5)){
+		mitad = (inf+sup)/2;
+		if(numeros[mitad] == dato){
+			band='V';
+			break;
+		}
+		if(numeros[mitad]>dato){
+			sup = mitad;
+			mitad = (inf+sup)/2;
+		}
+		if(numeros[mitad]<dato){
+			inf = mitad;
+			mitad = (inf+sup)/2;
+		}
+		i++;
+	}
+	
+	if(band == 'V'){
+		cout<<"El numero se encontro en la pos: "<<mitad<<endl;
+	}
+	else{
+		cout<<"El numero NO se encontro";
+	}
+	
+	getch();
+}
